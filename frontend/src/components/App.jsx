f6072b32bfb4af8c5ceadb79660003823ae555ed
Jaime de Venecia <deveneciaj@jdv-plus-2022.lan>
2023-07-28 20:10:01 -0700

    updates App.jsx to include secondary header

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <div id='app'>
       <h1>Nathan Peel's Cool Site</h1>
+
       <h1>This is a header</h1>
+      <h2>This is a secondary header</h2>
     </div>
   );
 };
