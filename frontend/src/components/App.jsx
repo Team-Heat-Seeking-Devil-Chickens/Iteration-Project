8fcee5546fb2bcf3bd1a8f8bfd592f795a05550f
Jaime de Venecia <deveneciaj@jdv-plus-2022.lan>
2023-07-28 19:44:00 -0700

    adds title to App component

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div id='app'></div>;
+  return (
+    <div id='app'>
+      <h1>Nathan Peel's Cool Site</h1>
+    </div>
+  );
 };
 
 export default App;
