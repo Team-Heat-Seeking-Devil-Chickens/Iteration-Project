d833234ee5f8a0c3b058e5e4e3381d16a3c46c2b
Kody M <kodylmontes@gmail.com>
2023-07-31 13:42:50 -0700

    Touching up the restaurant query container

--- frontend/src/containers/RestaurantQuery.jsx
+++ frontend/src/containers/RestaurantQuery.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateQuery } from '../features/querySlice';
 
-
-
 const RestaurantQuery = () => {
   // create an action for one drop-down
   const query = userSelector((state) => state.query);
@@ -11,33 +9,94 @@ const RestaurantQuery = () => {
 
   return (
     <div>
-      <input type='text' id='zipCode' />
-      <select name='Ambience' id='ambienceSelector' onChange={(e)=>dispatch(updateQuery(['ambience', e.target.value]))}>
+      <input
+        type='text'
+        id='restaurantName'
+        onChange={(e) => dispatch(updateQuery(['name', e.target.value]))}
+      />
+      <select
+        name='Cuisine'
+        id='cuisineSelector'
+        onChange={(e) => dispatch(updateQuery(['cuisine', e.target.value]))}
+      >
         <option value=''>select</option>
-        <option value='date_night'>Date Night</option>
+        <option value='Mexican'>Mexican</option>
+        <option value='Indian'>Indian</option>
+        <option value='American'>American</option>
+        <option value='Italian'>Italian</option>
+        <option value='Chinese'>Chinese</option>
+        <option value='Korean'>Korean</option>
+        <option value='Japanese'>Japanese</option>
+      </select>
+      <select
+        name='Ambience'
+        id='ambienceSelector'
+        onChange={(e) => dispatch(updateQuery(['ambience', e.target.value]))}
+      >
+        <option value=''>select</option>
+        <option value='date night'>Date Night</option>
         <option value='trendy'>Trendy</option>
         <option value='litty'>Litty</option>
         <option value='friendly'>Friendly</option>
         <option value='country'>Country</option>
       </select>
-      <select name='Cuisine' id='cuisineSelector' onChange={(e)=>dispatch(updateQuery(['cuisine', e.target.value]))}>
-        <option value='mexican'>Mexican</option>
-        <option value='indian'>Indian</option>
-        <option value='american'>American</option>
-        <option value='italian'>Italian</option>
-        <option value='chinese'>Chinese</option>
-        <option value='korean'>Korean</option>
-        <option value='japanese'>Japanese</option>
-      </select>
-      <select name='Price' id='priceSelector' onChange={(e)=>dispatch(updateQuery(['priceTier', e.target.value]))}>
+      <select
+        name='Price'
+        id='priceSelector'
+        onChange={(e) => dispatch(updateQuery(['priceTier', e.target.value]))}
+      >
+        <option value=''>select</option>
         <option value='exquisite'>Exquisite</option>
+        <option value='splurge'>Splurge</option>
+        <option value='affordable'>Affordable</option>
+        <option value='thrifty'>Thrifty</option>
+        <option value='dirt cheap'>Dirt Cheap</option>
       </select>
-      <input type='text' id='restaurantName' onChange={(e)=>dispatch(updateQuery(['name', e.target.value]))}/>
-      <switch id='vegetarianOptions'> </switch>
-      <select name='Location' id='locationRadius' onChange={(e)=>dispatch(updateQuery(['locationRad', e.target.value]))}></select>
-      <select name='Group' id='groupSizeSelector' onChange={(e)=>dispatch(updateQuery(['goodGroups', e.target.value]))}></select>
+      <Input
+        label='vegetarian/vegan options?'
+        type='checkbox'
+        name='plantBase'
+        id='plantBase'
+        format={(v) => v === '1'}
+        normalize={(v) => (v ? '1' : '0')}
+        onChange={(e) => dispatch(updateQuery(['plantBase', e.target.value]))}
+      />
+      <Input
+        label='good for groups?'
+        type='checkbox'
+        name='goodGroups'
+        id='goodGroups'
+        format={(v) => v === '1'}
+        normalize={(v) => (v ? '1' : '0')}
+        onchange={(e) => dispatch(updateQuery(['goodGroups', e.target.value]))}
+      />
+      <select
+        name='Location'
+        id='locationRadius'
+        onChange={(e) => dispatch(updateQuery(['locationRad', e.target.value]))}
+      >
+        <option value='5km'>5 km</option>
+        <option value='10km'>10 km</option>
+        <option value='15km'>15 km</option>
+        <option value='20km'>20 km</option>
+        <option value='25km'>25 km</option>
+      </select>
+      <select
+        name='Group'
+        id='groupSizeSelector'
+        onChange={(e) => dispatch(updateQuery(['goodGroups', e.target.value]))}
+      ></select>
     </div>
   );
 };
+/*
+Location input field removed
+ <input
+  type='text'
+  id='zipCode'
+  placeholder='zipcode'
+  onChange={(e) => dispatch(updateQuery(['zipCode', e.target.value]))}
+/> */
 
 // fields:
+export default RestaurantQuery;
--- frontend/src/features/querySlice.js
+++ frontend/src/features/querySlice.js
@@ -1,28 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-
+  name: '',
   ambience: '',
   cuisine: '',
   priceTier: '',
   plantBase: '',
   locationRad: '',
-  goodGroups: ''
-
-}
-
+  goodGroups: '',
+};
 
 export const querySlice = createSlice({
- name: 'query',
- initialState,
- reducers: {
-  updateQuery: (state, action) => {
-   // action payload should be an array [query attricute, value]
-   // invoke this function on onChange of the query selectors in Restaurant query --> i.e. onChange = (e) => dispatch(updateQuery(['ambience', e.target.value]))
-   //  may have to ensure that value is a num/bit ?
-    const attribute = action.payload[0];
-    const value = action.payload[1];
-    state[attribute] = value;
-  }
- }
-})
+  name: 'query',
+  initialState,
+  reducers: {
+    updateQuery: (state, action) => {
+      // action payload should be an array [query attricute, value]
+      // invoke this function on onChange of the query selectors in Restaurant query --> i.e. onChange = (e) => dispatch(updateQuery(['ambience', e.target.value]))
+      // if attribute is vegoptions or good fro groups, the data can be manipulated.
+      //  may have to ensure that value is a num/bit ?
+      const attribute = action.payload[0];
+      const value = action.payload[1];
+      state[attribute] = value;
+    },
+  },
+});
