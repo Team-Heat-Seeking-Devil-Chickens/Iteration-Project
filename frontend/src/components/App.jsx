904e1c33b4f463ad54c5fe4c900bfe7dad50052b
Trisha <trishanduong@gmail.com>
2023-07-31 10:04:48 -0700

    added reviewReducer, reviewContainer, some styling

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import reviewContainer from "../containers/ReviewContainer";
+import React, { useState } from "react";
+import "../styles/main.scss";
+import {useDispatch} from "react-redux"
+//import reviewContainer from "../containers/ReviewContainer";
+
+{
+  /* <h1>This is a header</h1>
+<h2>This is a secondary header</h2>
+<h3>This is a tertiary header</h3> */
+}
 
 const App = () => {
+
+
   return (
     <div id="app">
       <h1>Nathan Peel's Cool Site</h1>
-
-      <h1>This is a header</h1>
-      <h2>This is a secondary header</h2>
-
-      <h3>This is a tertiary header</h3>
     </div>
   );
 };
